8afe20d4cbdd9295bb48e28b4885962a0353a8f8
AndrewSMC1 <117210751+AndrewSMC1@users.noreply.github.com>
2023-05-04 00:20:56 -0700

    Added Google Maps

--- ArtistPages/Pages/Index.cshtml.cs
+++ ArtistPages/Pages/Index.cshtml.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ArtistPages.Pages;
 
 public class IndexModel : PageModel
 {
+    public string GoogleMapsAPIKey;
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
@@ -14,6 +14,15 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        DotNetEnv.Env.Load("../.env");
+
+        string MAPKEY = Environment.GetEnvironmentVariable("MAPKEY");
+        if (MAPKEY == null)
+        {
+            throw new Exception("MAPKEY environment variable is not set.");
+        }
+
+        GoogleMapsAPIKey = $"https://maps.googleapis.com/maps/api/js?key={MAPKEY}&callback=initMAP";
 
     }
 }
